1463300dfd5765ede7fc112d366c1012343915fd
Helixoo <104375707+Helixoo@users.noreply.github.com>
2024-04-23 19:28:37 +0300

    Update clqu.config.js

--- clqu.config.js
+++ clqu.config.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'Bark1n',
     version: '3.0.0',
     githubName: 'BrknYY',
-    githubKey: process.env.GITHUB_KEY,
+    githubKey: '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',
     softwareerKey: process.env.SOFTWAREER_KEY,
     email: 'no@clqu.me',
     errors: {
