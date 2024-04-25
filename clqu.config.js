5598540f7a4f092e8207731a9e7d0fbf7b27f7b5
Helixoo <104375707+Helixoo@users.noreply.github.com>
2024-04-25 20:05:20 +0300

    Update clqu.config.js

--- clqu.config.js
+++ clqu.config.js
@@ -2,9 +2,9 @@ module.exports = {
     name: 'Bark1n',
     version: '3.0.0',
     githubName: 'BrknYY',
-    githubKey: process.env.GITHUB_KEY,
+    githubKey: ${{ secrets.GT_KEY }},
     softwareerKey: process.env.SOFTWAREER_KEY,
-    email: 'no@clqu.me',
+    email: 'barkin@fbi.ac',
     errors: {
         404: "This page could not be found.",
         500: "An error occurred while processing your request."
